90b724b915588a4c46305eb97046bf68205e7ce8
yutosaito <scrft1234@icloud.com>
2021-04-23 18:16:39 +0900

    4/23

--- src/main/kotlin/Example.kt
+++ src/main/kotlin/Example.kt
@@ -1,3 +1,18 @@
 fun main(){
-    println("Hello kotlin")
+    val text =
+    """改行もインデントもこのとおり
+ああああ"""
+    println(text)
+    ifExample(100)
 }
+
+fun ifExample(num: Int){
+    if(num < 100){
+        println("Less than 100")
+    }else if (num == 100){
+        println("Equal to 100")
+    }else{
+        println("Greater then 100")
+    }
+}
+
